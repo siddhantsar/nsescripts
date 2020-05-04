8272e162bf77bfa5bff06d1a31880b6ff7ec1d49
Siddhant Saraswat <sidsar005@gmail.com>
2020-05-04 15:55:23 +0530

    time period selection feature added

--- historical_data_downloader/nse_csv.py
+++ historical_data_downloader/nse_csv.py
@@ -57,7 +57,7 @@ def create_driver(base_path):
     return driver
 
 
-def set_static_values(driver):
+def set_static_values(driver, time_period):
     """
     Method sets the values to dropdown menu items.
     These are static values which can be set once per session.
@@ -81,14 +81,18 @@ def set_static_values(driver):
             6. value="12month": 365 Days
             7. value="24month": 24 Months
     """
-    select_datatype = Select(driver.find_element_by_id("dataType"))
-    select_datatype.select_by_value("priceVolume")
+    try:
+        select_datatype = Select(driver.find_element_by_id("dataType"))
+        select_datatype.select_by_value("priceVolume")
 
-    select_series = Select(driver.find_element_by_id("series"))
-    select_series.select_by_value("EQ")
+        select_series = Select(driver.find_element_by_id("series"))
+        select_series.select_by_value("EQ")
 
-    select_time_period = Select(driver.find_element_by_id("dateRange"))
-    select_time_period.select_by_value("3month")
+        select_time_period = Select(driver.find_element_by_id("dateRange"))
+        select_time_period.select_by_value(time_period)
+    except Exception as e_exception:
+        print("Following error occured while saving static values: ")
+        print(e_exception)
 
 
 def set_symbol(driver, comp_symbol):
@@ -127,30 +131,61 @@ def download_csv(driver):
 
 if __name__ == "__main__":
     SYMBOL_INPUT = []
+    TIME_PERIOD = None
     print("NSE Historical Data Downloader")
-    option = input(
-        """Please select your choice:\n1. Single Stock Symbol (Average Response Time: 1-1.5secs)\n2. Nifty 50 List (Average Response Time: 50-55secs)\n3. Nifty 100 List (Average Response Time: 120-135secs)\n4. Nifty 500 List (Average Response Time: 9-10mins)\n
+    OPTION = input(
+        """Please select your choice:\n1. Single Stock Symbol (Average Response Time: 1-1.5secs)\n2. User Provided Stock Symbol List (Variable Response Time)\n3. Nifty 50 List (Average Response Time: 50-55secs)\n4. Nifty 100 List (Average Response Time: 120-135secs)\n5. Nifty 500 List (Average Response Time: 9-10mins)\n
 Your choice: """
     )
 
-    if option == "1":
+    if OPTION == "1":
         SYMBOL_INPUT.append(input("Please enter the correct stock symbol: "))
-    elif option == "2":
+    elif OPTION == "2":
+        SYMBOL_INPUT = input("Please enter SPACE separated stock symbols: ").split(" ")
+    elif OPTION == "3":
         print("Nifty 50 selected")
         SYMBOL_INPUT = stocks_lists.nifty_50
-    elif option == "3":
+    elif OPTION == "4":
         print("Nifty 100 selected")
         SYMBOL_INPUT = stocks_lists.nifty_100
-    elif option == "4":
+    elif OPTION == "5":
         print("Nifty 500 selected")
         SYMBOL_INPUT = stocks_lists.nifty_500
     else:
         print("Wrong input")
 
-    if SYMBOL_INPUT is not None:
+    OPTION = input(
+        """Please select desired time period:\n1. 01 day\n2. 07 days\n3. 15 days\n4. 01 month\n5. 03 months\n6. 12 months\n7. 24 months\n
+Your choice: """
+    )
+    if OPTION == "1":
+        print("Downloading past 1 day data")
+        TIME_PERIOD = "day"
+    elif OPTION == "2":
+        print("Downloading past 7 days data")
+        TIME_PERIOD = "week"
+    elif OPTION == "3":
+        print("Downloading last 15 days data")
+        TIME_PERIOD = "15days"
+    elif OPTION == "4":
+        print("Downloading past 1 month data")
+        TIME_PERIOD = "1month"
+    elif OPTION == "5":
+        print("Downloading past 3 months data")
+        TIME_PERIOD = "3month"
+    elif OPTION == "6":
+        print("Downloading past 12 months data")
+        TIME_PERIOD = "12month"
+    elif OPTION == "7":
+        print("Downloading past 24 months data")
+        TIME_PERIOD = "24month"
+    else:
+        print("Wrong input")
+
+    if SYMBOL_INPUT is not None and TIME_PERIOD is not None:
         BASE_PATH = os.path.abspath(os.getcwd())
         BROWSER = create_driver(BASE_PATH)
-        set_static_values(BROWSER)
+        set_static_values(BROWSER, TIME_PERIOD)
         START = timer()
         for symbol in SYMBOL_INPUT:
             set_symbol(BROWSER, symbol)
